cc89c04e11417284bbd2b266fb0291f454ccbdef
Jada Quandt <jadaquandt@gmail.com>
2021-06-10 21:31:23 -0400

    adding interceptors

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { environment as env } from 'src/environments/environment';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+    constructor() { }
+
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        req = req.clone({
+            setHeaders: {
+                'x-rapidapi-key': `${env.RAPID_API_KEY}`,
+                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+            },
+            setParams: {
+                key: `${env.API_KEY}`,
+            }
+        });
+        return next.handle(req);
+    }
+}
